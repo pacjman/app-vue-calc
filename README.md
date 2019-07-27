a5e271f21c6c36bc5463530c7550a30b06f1a78d
pacjman <31190203+pacjman@users.noreply.github.com>
2019-07-27 15:09:52 -0700

    add calculator demo based on vue & typescript template

--- README.md
+++ README.md
@@ -1,6 +1,32 @@
-# my-frist-vue-app
+# Calcular Demo
 
-> My frist! Vue.js project
+> Simple Calculator demo which performs basic maths using the power of Vue, TypeScript, and our minds.
+
+## Requirements
+
+> Create a calculator that can add, subtract, multiply, and divide. Don't worry about order of operations.
+> The calculator should display running history of calculations made. 
+> The history should be persistent across calculations.
+> Nice to have: the history should persist browser refresh.
+
+## Abstract
+
+* This is based from vue-calculator sample work per José Salazar here: https://codepen.io/jsalazart
+* The example was modified to meet the above requirements and styled so the history looks similar to a running tape.
+* There were a number of buggy behaviors in the sample. See test cases for more details.
+
+## Test Cases
+
+1. Upon launching app, ensure the display starts with 0. Ensure it displays the minimum viable operators specified in requirements.
+2. Perform a number of random calculations (e.g. 3*3, 15-4, etc) hit = to evaluate each run, confirm the correct maths reported.
+3. Assuming previous test cases pass, ensure the running history logs the successful calculations.
+4. Nice to have - Ensure the modulus operator ("%") and decimal operator are shown (".")
+5. Nice to have - Ensure the C function clears immediate display to 0.
+6. Nice to have - Assuming previous test passes, refresh browser, and confirm the running history is still resident.
+7. Nice to have - Assuming previous test passes, ensure the AC function clears the immediate and running history areas.
+8. Edge case - Hit AC, enter operator without providing number, ensure the display remains unaffected.
+9. Edge case - Hit AC, enter some number value followed by an operator (e.g. "*") then immediately click a different operator (e.g. "/"). Expect the operator is replaced (e.g. * becomes /)
+10. Edge case - Provide an incomplete expression (e.g. 13 *) then click = to evaluate, confirm no calculation occurred, and nothing logged to running history.
 
 ## Build Setup
 
@@ -11,10 +37,7 @@ npm install
 # build for development
 npm run build
 
-# build for production w/ minification
-npm run deploy
-
-# serve with hot reload @ localhost:8080
+# serve using webpack dev
 npm run dev
 
 # clean up dependencies (requires git)
@@ -24,29 +47,20 @@ npm run clean
 ## System Dependencies
 
 ``` bash
-# npm version: 3.5.2
+# npm version: 6.9.0
 npm --version
 
-# node version: 8.10.0
+# node version: 12.6.0
 node --version
 
-# git version: git version 2.17.1
+# git version: git version 2.20.1 (Apple Git-117)
 git --version
 ```
 
-### Recommended System
+### References
 
-* Linux Mint 19 "Tara". [Click here to download](https://linuxmint.com/download.php).
-
-### Recommended Dev Tools
-
-* Microsoft Visual Studio Code. [Click here to download](https://code.visualstudio.com/Download).
-* ScooterSoft Beyond Compare. [Click here to download free 30-day trial](http://www.scootersoftware.com/download.php).
-
-### Other Resources
-
-* [Introduction to Vue.js](https://vuejs.org/v2/guide/).
-* [Consult the docs for vue-loader](http://vuejs.github.io/vue-loader).
-* [Getting Started: Vue.js and Visual Studio Code](https://itnext.io/getting-started-vue-js-and-visual-studio-code-6990f92e918a).
+* [José Salazar's Vue Calculator from CodePen](https://codepen.io/jsalazart/pen/jJWMVw).
+* [Installing Node & NPM on a Mac](https://treehouse.github.io/installation-guides/mac/node-mac.html).
+* [Using the dev tools for Safari on Mac](https://support.apple.com/guide/safari/use-the-developer-tools-in-the-develop-menu-sfri20948/mac).
 * [Microsoft TypeScript Vue Starter](https://github.com/Microsoft/TypeScript-Vue-Starter).
 * [John Papa: Vue.js with TypeScript](https://johnpapa.net/vue-typescript/).
--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,324 @@
 
 <template>
   <div id="app">
-    <h1>{{ title }}</h1>
-    <div class="greeting"><span>Greetings</span></div>
+    <h1>Calculator Demo</h1>
+
+    <!--  https://github.com/salazarr-js/v-calculator  -->
+
+    <div id="calculator">
+
+      <div>
+        <img width="320" src="pacjman.jpg"/>
+      </div>
+
+      <div class="calculator-logs">
+        <span v-for="log in logs">{{ log }}</span>
+      </div>
+
+      <input type="string" class="calculator-input" v-model="value" @keyup.enter="pop()">
+
+      <div class="calculator-row">
+        <div class="calculator-col">
+          <button class="calculator-btn gray action" @click="reset()">AC</button>
+        </div>
+        <div class="calculator-col">
+          <button class="calculator-btn gray action" @click="clear()">C</button>
+        </div>
+        <div class="calculator-col">
+          <button class="calculator-btn gray action" @click="push('%')">%</button>
+        </div>
+        <div class="calculator-col">
+          <button class="calculator-btn accent action" @click="push('/')">÷</button>
+        </div>
+      </div>
+
+      <div class="calculator-row">
+        <div class="calculator-col">
+          <button class="calculator-btn" @click="push(7)">7</button>
+        </div>
+        <div class="calculator-col">
+          <button class="calculator-btn" @click="push(8)">8</button>
+        </div>
+        <div class="calculator-col">
+          <button class="calculator-btn" @click="push(9)">9</button>
+        </div>
+        <div class="calculator-col">
+          <button class="calculator-btn accent action" @click="push('*')">x</button>
+        </div>
+      </div>
+
+      <div class="calculator-row">
+        <div class="calculator-col">
+          <button class="calculator-btn" @click="push(4)">4</button>
+        </div>
+        <div class="calculator-col">
+          <button class="calculator-btn" @click="push(5)">5</button>
+        </div>
+        <div class="calculator-col">
+          <button class="calculator-btn" @click="push(6)">6</button>
+        </div>
+        <div class="calculator-col">
+          <button class="calculator-btn accent action" @click="push('-')">-</button>
+        </div>
+      </div>
+
+      <div class="calculator-row">
+        <div class="calculator-col">
+          <button class="calculator-btn" @click="push(1)">1</button>
+        </div>
+        <div class="calculator-col">
+          <button class="calculator-btn" @click="push(2)">2</button>
+        </div>
+        <div class="calculator-col">
+          <button class="calculator-btn" @click="push(3)">3</button>
+        </div>
+        <div class="calculator-col">
+          <button class="calculator-btn accent action" @click="push('+')">+</button>
+        </div>
+      </div>
+
+      <div class="calculator-row">
+        <div class="calculator-col wide">
+          <button class="calculator-btn" @click="push(0)">0</button>
+        </div>
+        <div class="calculator-col">
+          <button class="calculator-btn action" @click="push('.')">.</button>
+        </div>
+        <div class="calculator-col">
+          <button class="calculator-btn accent action" @click="pop()">=</button>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
-import Component from 'vue-class-component';
-@Component({
-  components: { }
-})
-export default class App extends Vue {
-  title: string;
-  constructor() {
-    super();
-    this.title = 'My Frist Vue App';
+
+  import Vue from 'vue';
+  import Component from 'vue-class-component';
+
+  @Component({
+    components: { }
+  })
+
+  export default class App extends Vue {
+    
+    expr: string = '';
+    logs: string[] = [];
+
+    constructor() {
+      super();
+
+      this.expr = '';
+      this.logs = LogRepo.Get();
+    }
+
+    reset(): void {
+
+      this.expr = '';
+      this.logs = [];
+
+      LogRepo.Put(this.logs);
+    }
+
+    push(e: string): void {
+
+      if ( Util.IsNumber(e) ) {
+
+        if ( this.expr.length == 0 ) {
+          this.expr = e.toString();
+        } else {
+          this.expr += e.toString();
+        }
+
+      } else {
+
+        if ( this.expr.length == 0) {
+          return;
+        } else {
+
+          let prev = this.expr[this.expr.length - 1];
+          if ( Util.IsOperator(prev) ) {
+            this.expr = this.expr.slice(0, -1);
+          }
+            
+          this.expr += e.toString();
+        }
+      }
+    }
+
+    pop(): void {
+
+      let log: string = this.expr;
+      this.expr = eval(this.expr).toString();
+
+      if (log != this.expr) {
+        this.logs.push( log + `=${this.expr}` );
+        LogRepo.Put(this.logs);
+      }
+
+    }
+
+    clear(): void {
+      this.expr = '';
+    }
+    
+    get value(): string {
+
+      return this.expr.length > 0
+        ? this.expr
+        : "0"
+        ;
+
+    }
+
   }
-}
+
+  class Util {
+
+    static IsNumber(e: any): boolean {
+      return ( Number.isInteger(e) );
+    }
+
+    static IsOperator(e: string): boolean {
+      return ( e == "%" || e == "/" || e == "*" || e == "-" || e == "." );
+    }
+
+  }
+
+  class LogRepo {
+
+    static Get(): string[] {
+
+      let logStr = window.localStorage.getItem('logs');
+      if (logStr === null) {
+        LogRepo.Put([]);
+        return [];
+      }
+
+      let logObj = JSON.parse(logStr);
+      return logObj;
+
+    }
+
+    static Put(value : string[]) {
+
+      let logStr = JSON.stringify(value);
+      window.localStorage.setItem('logs', logStr);
+
+    }
+
+  }
+
+
 </script>
 
 <style>
 
-  .greeting {
-    font-size: 40px;
+  *, ::after, ::before {
+    box-sizing: border-box;
+  }
+
+  body {
+    margin: 0;
+    height: 100vh;
+    display: flex;
+    font-size: 10px;
+    align-items: flex-end;
+    justify-content: center;
+    background-color: #333;
+  }
+
+  #calculator {
+    width: 100%;
+    margin: 0 auto;
+    display: flex;
+    padding: 0;
+    max-width: 320px;
+    min-width: 320px;
+    flex-direction: column;
+    background-color: #2f2f31;
+  }
+
+  #calculator .calculator-logs {
+    height: 80px;
+    display: flex;
+    position: relative;
+    overflow: hidden;
+    align-items: flex-end;
+    flex-direction: column;
+    justify-content: flex-end;
+    background-color: #ccc;
+  }
+  #calculator .calculator-logs:before {
+    top: 0;
+    left: 0;
+    right: 0;
+    height: 48px;
+    content: '';
+    z-index: 5;
+    position: absolute;
+    background: linear-gradient(to bottom, #2f2f31, rgba(47, 47, 49, 0));
+  }
+  #calculator .calculator-logs span {
+    color: #333;
+    opacity: .75;
+    display: block;
+    font-size: .8rem;
+    text-align: right;
+    margin-top: .4rem;
+    line-height: 1;
+    font-weight: lighter;
+  }
+  #calculator .calculator-input {
+    color: #333;
+    width: 100%;
+    border: none;
+    padding: .8rem;
+    display: block;
+    font-size: 2.4rem;
+    background-color: #b3ffe0;
+    text-align: right;
+    font-weight: lighter;
+  }
+  #calculator .calculator-input:focus, #calculator .calculator-input:active {
+    outline: none;
+  }
+  #calculator .calculator-row {
+    display: flex;
+    padding: 0;
+    justify-content: space-around;
+  }
+  #calculator .calculator-row .calculator-col {
+    flex: 1;
+    box-shadow: 0 0 0 1px #2f2f31;
+  }
+  #calculator .calculator-row .calculator-col.wide {
+    flex: 2;
+  }
+  #calculator .calculator-btn {
+    width: 100%;
+    color: #D4D4D2;
+    border: none;
+    cursor: pointer;
+    padding: .8rem;
+    outline: none;
+    font-size: 1.6rem;
+    transition: all .3s ease-in-out;
+    font-weight: 200;
+    justify-content: center;
+    background-color: #616163;
+  }
+  #calculator .calculator-btn.accent {
+    background-color: #f49e3f;
+    color: #fff;
+  }
+  #calculator .calculator-btn.gray {
+    background-color: #424345;
+  }
+  #calculator .calculator-btn:active {
+    background-color: #2f2f31;
   }
 
 </style>
